ddff2686a8526603ca8f3eea6081cf99343c9177
noordseoostende <voropayev1@mail.ru>
2021-05-20 15:48:51 +0200

    het zoeken is mogelijk

---
+++ api/search.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const router=express.Router();
+const authMiddleware=require('../middleware/authMiddleware');
+const UserModel=require('../models/UserModel');
+
+removeListener.get('/:searchText', async(req, res) => {
+  const {searchText} = req.params;
+
+  if(searchText.length === 0) return;
+
+  try {
+    let userPattern = new RegExp(`^${searchText}`);
+
+    const results = await UserModel.find({
+      name: { $regex: userPattern, $options: "i" }
+    });
+
+    res.json(results);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+})
