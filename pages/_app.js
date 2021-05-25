042760f38b004b12a476c4107d943a247c668cbe
noordseoostende <voropayev1@mail.ru>
2021-05-25 11:58:54 +0200

    notifications

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,10 @@ class MyApp extends App {
     const { token } = parseCookies(ctx);
     let pageProps = {};
 
-    const protectedRoutes = ctx.pathname === "/" || ctx.pathname === "/[username]";
+    const protectedRoutes = ctx.pathname === "/" || 
+    ctx.pathname === "/[username]" ||
+    ctx.pathname === "/notifications" ||
+    ctx.pathname === "/post/[postId]";
 
     if (!token) {
       protectedRoutes && redirectUser(ctx, "/login");
